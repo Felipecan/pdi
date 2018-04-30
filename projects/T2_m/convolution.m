911550cd94536665aa290d7db0ac54da004f3d30
Felipecan <felipe.cunha@eng.ci.ufpb.br>
2018-04-30 15:51:27 -0300

    correções

--- projects/T2_m/convolution.m
+++ projects/T2_m/convolution.m
@@ -1,37 +1,58 @@
 function out_img = convolution(in_img, mat)
     
-    out_img = in_img;%zeros(size(in_img));
-
-    %rebatimento
-    temp = mat(:, 3);
-    mat(:, 3) = mat(:, 1);
-    mat(:, 1) = temp;
+    %out_img = in_img;%zeros(size(in_img));   
+    in_img_bordes0 = zeros( size(in_img,1)+(floor(size(mat,1)/2))*2, size(in_img,2)+(floor(size(mat,2)/2))*2, size(in_img,3) );    
+    in_img_bordes0(ceil(size(mat,1)/2):(size(in_img_bordes0,1)-floor(size(mat,1)/2)), ceil(size(mat,2)/2):(size(in_img_bordes0,2)-floor(size(mat,2)/2)), 1:size(in_img,3)) = in_img;   
+    %in_img_bordes0 = uint8(in_img_bordes0);
+    out_img = in_img_bordes0;
     
-    temp = mat(3, :);
-    mat(3, :) = mat(1, :);
-    mat(1, :) = temp;
-   
-    for r = ceil( size(mat, 1)/2 ): size(in_img, 1)-floor( size(mat, 1)/2 )       
-        for c = ceil( size(mat, 2)/2 ): size(in_img, 2)-floor( size(mat, 1)/2 )
-           sub_matrix = in_img(r - floor( size(mat, 1)/2 ): (r - floor( size(mat, 1)/2 ))+(size(mat, 1)-1) , c - floor( size(mat, 2)/2 ): (c - floor( size(mat, 2)/2 ))+(size(mat, 2)-1), 1:3);
-           
-           temp_mat = zeros(size(sub_matrix));
-           for i = 1: size(mat, 1)
-               for j = 1: size(mat, 2)
-                   temp_mat(i, j, 1) = temp_mat(i, j, 1) + (sub_matrix(i, j, 1) * mat(i, j));
-                   temp_mat(i, j, 2) = temp_mat(i, j, 2) + (sub_matrix(i, j, 2) * mat(i, j));
-                   temp_mat(i, j, 3) = temp_mat(i, j, 3) + (sub_matrix(i, j, 3) * mat(i, j));
-               end
-           end
+    %rebatimento
+%     temp = mat(:, 3);
+%     mat(:, 3) = mat(:, 1);
+%     mat(:, 1) = temp;
+%     
+%     temp = mat(3, :);
+%     mat(3, :) = mat(1, :);
+%     mat(1, :) = temp;
+
+    m_kernel = mat(:)/(size(mat,1)*size(mat,2));
+
+    for r = ceil( size(mat, 1)/2 ): size(in_img_bordes0, 1)-floor( size(mat, 1)/2 )       
+        for c = ceil( size(mat, 2)/2 ): size(in_img_bordes0, 2)-floor( size(mat, 2)/2 )
+%             disp("pegando matriz de: ")
+%             disp(r - floor( size(mat, 1)/2 ))
+%             disp("ate")
+%             disp((r - floor( size(mat, 1)/2 ))+(size(mat, 1)-1))
+%             disp("E: ")
+%             disp(c - floor( size(mat, 2)/2 ))
+%             disp((c - floor( size(mat, 2)/2 ))+(size(mat, 2)-1))
+            sub_matrix = in_img_bordes0(r - floor( size(mat, 1)/2 ): (r - floor( size(mat, 1)/2 ))+(size(mat, 1)-1) , c - floor( size(mat, 2)/2 ): (c - floor( size(mat, 2)/2 ))+(size(mat, 2)-1), 1:size(in_img,3));
+            
+            temp_mat = zeros(size(sub_matrix));
+            for i = 1: size(mat, 1)
+                for j = 1: size(mat, 2)
+                    for k = 1: size(in_img, 3)
+                        temp_mat(i, j, k) = sub_matrix(i, j, k) * mat(i, j);
+                    end
+                end
+            end
           
-          temp = temp_mat(:,:,1);
-          out_img(r, c, 1) = sum(temp(:));
-          temp = temp_mat(:,:,2);
-          out_img(r, c, 2) = sum(temp(:));
-          temp = temp_mat(:,:,3);
-          out_img(r, c, 3) = sum(temp(:));
+            for i = 1:size(in_img, 3)
+                temp = temp_mat(:,:,i);
+                temp = sum(temp(:));
+%                 if temp < 0
+%                     out_img(r, c, i) = 0;
+%                 elseif temp > 255
+%                     out_img(r, c, i) = 255;
+%                 else
+%                     out_img(r, c, i) = temp;
+%                 end
+               
+                  out_img(r, c, i) = temp;
+               
+            end         
+          
         end
     end
     
-    %disp(out_img)
 end
--- projects/T2_m/main.m
+++ projects/T2_m/main.m
@@ -9,11 +9,14 @@ file_path = input(prompt, 's');
 
 if ~isempty(img_path) || ~isempty(img_file)
     img = imread(img_path);
-    mat = [-1 -1 -1; -1 8 -1; -1 -1 -1];%dlmread(file_path);
-    out_img = conv2(single(img), single(mat));%convolution(img, mat);
-    imshow(out_img)
-    %subplot(1, 2, 1), imshow(img)
-    %subplot(1, 2, 2), imshow(out_img)
+    mat = dlmread(file_path);
+    
+    out_img = convolution(rgb2gray(img), mat);
+    out = conv2(rgb2gray(img), mat);
+    
+    subplot(1, 3, 1), imshow(img)
+    subplot(1, 3, 2), imshow(out_img)
+    subplot(1, 3, 3), imshow(out)
 else
     disp("Enter a path for the image and/or the matrix file.")
 end
